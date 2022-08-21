70f3f9431e1f94147192c7f0331c3831bb3f3f7f
LuciaPaciello <mlucia.paciello@gmail.com>
2022-08-21 18:36:09 -0300

    Feature: programs post type creation

--- wp-content/mu-plugins/university-post-type.php
+++ wp-content/mu-plugins/university-post-type.php
@@ -5,6 +5,9 @@
 // accesibilidad dependerá de que el tema esté activo. 
 
 function university_post_types() {
+
+  // Event post type:
+
   register_post_type('event', array(
     // Con el show_in_rest se mostrará el modern block editor.
     'show_in_rest' => true,
@@ -22,6 +25,27 @@ function university_post_types() {
       'singular_name' => 'Event'
     ),
     'menu_icon' => 'dashicons-calendar'
+  ));
+
+  // Program post type:
+
+  register_post_type('program', array(
+    // Con el show_in_rest se mostrará el modern block editor.
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    // Lo que se verá en la URL: 
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    // Será visto por los visitantes de la página. 
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
   ));
 }
 
